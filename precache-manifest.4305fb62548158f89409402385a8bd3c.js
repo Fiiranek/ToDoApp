714c665a7fea5d39f90748f153c1b2cd620d7806
Fiiranek <jellybean@onet.eu>
2020-07-06 16:11:42 +0200

    Updates

---
+++ precache-manifest.4305fb62548158f89409402385a8bd3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0e8d67012fdec64b437474862e7474",
+    "url": "/Fiiranek/ToDoApp/index.html"
+  },
+  {
+    "revision": "7ac5c6c930d28b40d7d5",
+    "url": "/Fiiranek/ToDoApp/static/css/main.4674f602.chunk.css"
+  },
+  {
+    "revision": "b068fae49a61a71e7d77",
+    "url": "/Fiiranek/ToDoApp/static/js/2.f52aff58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fiiranek/ToDoApp/static/js/2.f52aff58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac5c6c930d28b40d7d5",
+    "url": "/Fiiranek/ToDoApp/static/js/main.117d3bd5.chunk.js"
+  },
+  {
+    "revision": "09ce0f42d250abc70417",
+    "url": "/Fiiranek/ToDoApp/static/js/runtime-main.a55fd99d.js"
+  }
+]);
